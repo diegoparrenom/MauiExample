3e1d2dfd0d4630ac02c54f3cf0444d4f5318aa46
dparrenom <diegoparrenom@gmail.com>
2024-09-22 13:39:14 -0500

    Primer commit

---
+++ MonkeyFinder/AppShell.xaml.cs
@@ -0,0 +1,12 @@
+namespace MonkeyFinder;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+
+		//nameof(DetailsPage) == "DetailsPage"
+		Routing.RegisterRoute(nameof(DetailsPage), typeof(DetailsPage));
+	}
+}
---
+++ MonkeyFinder/ViewModel/BaseViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace MonkeyFinder.ViewModel;
+
+public partial class BaseViewModel : ObservableObject
+{
+    public BaseViewModel()
+    {
+
+    }
+    [ObservableProperty]
+    //[AlsoNotifyChangeFor(nameof(IsNotBusy))]
+    [NotifyPropertyChangedFor(nameof(IsNotBusy))]
+    bool isBusy;
+
+    [ObservableProperty]
+    string title;
+
+    public bool IsNotBusy => !IsBusy;
+
+}
